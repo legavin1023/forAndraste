2c0c2ec4dfc480b965cf0c978d3a2e1a5c9b9cfb
huiseon <legavin1023@gmail.com>
2023-09-29 13:26:25 +0900

    수정

--- src/main.js
+++ src/main.js
@@ -7,15 +7,20 @@ import "@/assets/css/reset.css";
 import "@/assets/scss/index.scss";
 import "animate.css";
 import VueGtm from "vue-gtm";
+import VueAnalytics from "vue-analytics";
 
+Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: "G-QCVE085BGW", // 여러분의 Google Analytics 추적 ID로 교체하세요.
+  router,
+});
 Vue.use(VueGtm, {
   id: "GTM-TLP29MTB", // GTM 컨테이너 ID를 입력하세요
   enabled: true,
   debug: true,
   vueRouter: router, // Vue Router 인스턴스, 필요하면
 });
-
-Vue.config.productionTip = false;
 
 new Vue({
   router,
